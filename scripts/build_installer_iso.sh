e7b8aca9740ae277af4dabd95dad8a96f2053ac6
root <root@test.eve-ng.com>
2016-11-23 01:35:58 -0500

    iso build final setup fix

--- scripts/build_installer_iso.sh
+++ scripts/build_installer_iso.sh
@@ -12,6 +12,7 @@ mount -o loop /tmp/ubuntu-16.04-server-amd64.iso /tmp/ubuntu-iso
 rsync -av /tmp/ubuntu-iso/ /tmp/eve-iso
 cp /usr/src/eve-ng-public-dev/debian/txt.cfg /tmp/eve-iso/isolinux/
 cp /usr/src/eve-ng-public-dev/debian/eve*.seed /tmp/eve-iso/preseed/
+cp /usr/src/eve-ng-public-dev/debian/eve-setup.sh /tmp/eve-iso/install/
 IMAGE=/tmp/EVE.iso BUILD=/tmp/eve-iso
 mkisofs -r -V "EVE Install CD"             -cache-inodes             -J -l -b isolinux/isolinux.bin             -c isolinux/boot.cat -no-emul-boot             -boot-load-size 4 -boot-info-table             -o $IMAGE $BUILD
 #rm -fr /tmp/eve-iso
