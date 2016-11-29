3d0833a96fc190526396cb19e89c32378ad1e12d
root <root@unl01.example.com>
2016-11-29 09:15:53 +0100

    Adding migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -55,31 +55,77 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-# Migrating labs
-echo "IMPORTING LABS:"
+# Installing requirements
+sshpass -e ssh root@${SRC} apt-get -qy install rsync &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: cannot install rsync to ${SRC}"
+	exit 1
+fi
+
+# Importing FQDN and hosts
+sshpass -e scp root@${SRC}:/etc/hostname /etc/hostname &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed import hostname"
+	exit 1
+fi
+sshpass -e scp root@${SRC}:/etc/hosts /etc/hosts &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed import hosts file"
+	exit 1
+fi
+hostnamectl set-hostname $(cat /etc/hostname) &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed to set local hostname"
+	exit 1
+fi
+
+# Importing labs
+echo "---------------------------------------------------------------------------"
+echo " IMPORTING LABS"
+echo "---------------------------------------------------------------------------"
 sshpass -e rsync -av -e ssh root@${SRC}:/opt/unetlab/labs /opt/unetlab 
 if [ $? -ne 0 ]; then
 	echo "ERROR: failed to import labs"
 	exit 1
 fi
 
-# Migrating tmp
-echo "IMPORTING ACTIVE LABS:"
+# Importing tmp
+echo "---------------------------------------------------------------------------"
+echo " IMPORTING ACTIVE LABS"
+echo "---------------------------------------------------------------------------"
 sshpass -e rsync -av -e ssh root@${SRC}:/opt/unetlab/tmp /opt/unetlab 
 if [ $? -ne 0 ]; then
 	echo "ERROR: failed to import active labs"
 	exit 1
 fi
 
-# Migrating addons
-echo "IMPORTING ADDONS:"
+# Importing addons
+echo "---------------------------------------------------------------------------"
+echo " IMPORTING ADDONS"
+echo "---------------------------------------------------------------------------"
 sshpass -e rsync -av -e ssh root@${SRC}:/opt/unetlab/addons /opt/unetlab 
 if [ $? -ne 0 ]; then
 	echo "ERROR: failed to import addons"
 	exit 1
 fi
 
-# Migrating users
+# Importing users
+echo "---------------------------------------------------------------------------"
+echo " IMPORTING USERS"
+echo "---------------------------------------------------------------------------"
+sshpass -e scp root@${SRC}:/opt/unetlab/data/database.sdb /tmp/database.sdb &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed to import database.sdb"
+	exit 1
+fi
+echo "SELECT password FROM users WHERE username = 'admin';" 2> /dev/null | sqlite3 /tmp/database.sdb 2> /dev/null | sed 's/^\(.*\)$/UPDATE users SET password = "\1" WHERE username = "admin";/g' 2> /dev/null | mysql -u eve-ng --password=eve-ng eve_ng_db &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "ERROR: failed to update admin password"
+	exit 1
+fi
 
 # Hostname
+echo "---------------------------------------------------------------------------"
+echo " MIGRATION COMPLETED"
+echo "---------------------------------------------------------------------------"
 
